8c1275872efa9fa89dc5874f6f4f3f992be238ef
mfthn <fathincbt3@gmail.com>
2025-03-24 13:16:04 +0700

    adjust topnav

--- src/components/topnav.tsx
+++ src/components/topnav.tsx
@@ -26,14 +26,14 @@ export function TopNav() {
 
   return (
     <div className="relative w-full border-b-4 bg-[#f3f3f3]" style={navHeight}>
-      <div className="absolute inset-0 w-full h-full">
+      <div className="absolute inset-0 w-full h-full overflow-hidden">
         <Image 
-          src="/images/topnav3.png" 
+          src="/images/topnav5.png" 
           alt="Background" 
-          width={1920}
-          height={120}
-          className="w-full h-full object-contain object-center"
+          fill={true}
+          style={{ objectFit: 'contain' }}
           priority
+          sizes="100vw"
         />
       </div>
       <nav className="relative flex w-full h-full items-center justify-between px-6 py-3 text-xl font-semibold z-10">
@@ -51,8 +51,8 @@ export function TopNav() {
             {mahasiswaData.data && (
               <div className="flex flex-row items-center gap-5">
                 <div className="flex flex-col text-right">
-                  <p className="text-gray-800 mb-1 text-base font-medium">{mahasiswaData.data.name}</p>
-                  <p className="text-gray-700 text-sm">{mahasiswaData.data.nim}</p>
+                  <p className="text-gray-800 mb-1 text-lg font-medium">{mahasiswaData.data.name}</p>
+                  <p className="text-gray-700 text-base font-medium">{mahasiswaData.data.nim}</p>
                 </div>
                 <Button
                   type="button"
